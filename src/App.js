fd1643f644af79d91ee3a733a5e46f55522062de
chundong <chundong@chundongui-MacBookAir.local>
2021-07-05 23:52:47 +0900

    Link 태그 적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import "./App.css";
 import MainPageComponent from "./main/index.js";
+import TimerComponent from "./timer";
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
 
 function App() {
-  return <MainPageComponent />;
+  // return <TimerComponent />;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path={"/"}>
+          <MainPageComponent />
+        </Route>
+        <Route exact={true} path="/product/:id">
+          <ProductPage />
+        </Route>
+        <Route exact={true} path="/upload">
+          <UploadPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./index.css";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 function MainPage() {
   const [products, setProducts] = React.useState([]);
@@ -10,6 +11,7 @@ function MainPage() {
         "https://601ed9c0-fe7b-43c5-bb0b-13a8b4d59eed.mock.pstmn.io/products"
       )
       .then(function (result) {
+        console.log(result);
         const products = result.data.products;
         setProducts(products);
       })
@@ -29,25 +31,28 @@ function MainPage() {
         <div id="banner">
           <img src="images/banners/banner1.png" />
         </div>
-        <h1>판매되는 상품들</h1>
+        <h1>판매되는 상품들 </h1>
         <div id="product-list">
           {products.map(function (product, index) {
             return (
               <div className="product-card">
-                <div>
-                  <img className="product-img" src={product.imageUrl} />
-                </div>
-                <div className="product-contents">
-                  <span className="product-name">{product.name}</span>
-                  <span className="product-price">{product.price}원</span>
-                  <div className="product-seller">
-                    <img
-                      className="product-avatar"
-                      src="images/icons/avatar.png"
-                    />
-                    <span>{product.seller}</span>
+                <Link className="product-link" to={`/product/${index}`}>
+                  {/* Link는 실제로 html 중 a 태그로 사용된다 따라서 클릭하면 문자의 색깔이 자주색으로 바뀜 */}
+                  <div>
+                    <img className="product-img" src={product.imageUrl} />
                   </div>
-                </div>
+                  <div className="product-contents">
+                    <span className="product-name">{product.name}</span>
+                    <span className="product-price">{product.price}원</span>
+                    <div className="product-seller">
+                      <img
+                        className="product-avatar"
+                        src="images/icons/avatar.png"
+                      />
+                      <span>{product.seller}</span>
+                    </div>
+                  </div>
+                </Link>
               </div>
             );
           })}
